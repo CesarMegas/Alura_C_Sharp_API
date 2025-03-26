99e6f0bde00edfa397cc728584e1b0dc59700a9a
César Nagy <103067057+CesarMegas@users.noreply.github.com>
2025-03-26 18:46:41 -0300

    Song.cs
    Programa responsável pela Definição e Exibição das Músicas do Projeto Principal.

---
+++ MelodyFlowAPI/Models/Song.cs
@@ -0,0 +1,35 @@
+using System.Text.Json.Serialization;
+
+namespace MelodyFlowAPI.Models;
+
+internal class Song
+{
+    [JsonPropertyName("artist")]
+    public string? Artist { get; set; }
+
+    [JsonPropertyName("song")]
+    public string? Name { get; set; }
+
+    [JsonPropertyName("duration_ms")]
+    public int Duration { get; set; }
+
+    [JsonPropertyName("explicit")]
+    public string? Explicit { get; set; }
+
+    [JsonPropertyName("year")]
+    public string? Year { get; set; }
+
+    [JsonPropertyName("genre")]
+    public string? Genre { get; set; }
+
+    public void DisplaySongDetails()
+    {
+        Console.WriteLine($"Artist: {Artist}");
+        Console.WriteLine($"Song: {Name}");
+        Console.WriteLine($"Duration in Seconds: {Duration / 1000}");
+        var explicitContent = Explicit == "True" ? "Yes" : "No";
+        Console.WriteLine($"Contains Explicit Content: {explicitContent}");
+        Console.WriteLine($"Published in: {Year}");
+        Console.WriteLine($"Genre: {Genre}");
+    }
+}
