7daa02fb0710159745b36289f3c11843f35352f3
César Nagy <103067057+CesarMegas@users.noreply.github.com>
2025-03-31 15:12:21 -0300

    Program.cs
    Programa Principal do Projeto.

--- MelodyFlowAPI/Program.cs
+++ MelodyFlowAPI/Program.cs
@@ -1,3 +1,7 @@
+using MelodyFlowAPI.Filters;
+using MelodyFlowAPI.Models;
+using System.Text.Json;
+
 // Using HTTP API to get the songs
 using (HttpClient client = new HttpClient())
 {
@@ -5,7 +9,13 @@ using (HttpClient client = new HttpClient())
     try
     {
         string response = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        Console.WriteLine(response);
+        var songs = JsonSerializer.Deserialize<List<Song>>(response)!;
+        //songs[1998].DisplaySongDetails();
+        //LinqFilter.FilterEveryMusicalGenre(songs);
+        //LinqOrder.DisplayOrderedArtistsList(songs);
+        //LinqFilter.FilterArtistsByMusicalGenre(songs, "pop");
+        //LinqFilter.FilterSongsOfAnArtist(songs, "Michael Jackson");
+        LinqFilter.FilterSongsByYear(songs, 2000);
     }
     // If it doen't succeed, displays the error
     catch (Exception error) 
