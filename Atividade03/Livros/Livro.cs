d2cc8355e17c8e18c03831cb1e917f7e43e65e3b
César Nagy <103067057+CesarMegas@users.noreply.github.com>
2025-03-31 15:10:13 -0300

    Livro.cs
    Programa responsável pela criação dos Livros da Atividade 03.

---
+++ Atividade03/Livros/Livro.cs
@@ -0,0 +1,8 @@
+namespace Atividade03.Livros;
+
+internal class Livro
+{
+    public string Nome { get; set; }
+    public string Autor { get; set; }
+    public int AnoDePublicacao { get; set; }
+}
