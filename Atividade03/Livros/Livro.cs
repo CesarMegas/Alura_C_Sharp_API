158a3c815a8393f1d64ac7245d9fad1b4789846d
César Nagy <103067057+CesarMegas@users.noreply.github.com>
2025-04-02 17:36:40 -0300

    Delete Atividade03 directory

--- Atividade03/Livros/Livro.cs
+++
@@ -1,8 +0,0 @@
-namespace Atividade03.Livros;
-
-internal class Livro
-{
-    public string Nome { get; set; }
-    public string Autor { get; set; }
-    public int AnoDePublicacao { get; set; }
-}
--- Atividade03/Produtos/Produto.cs
+++
@@ -1,7 +0,0 @@
-namespace Atividade03.Produtos;
-
-internal class Produto
-{
-    public string Nome { get; set; }
-    public decimal Preco { get; set; }
-}
