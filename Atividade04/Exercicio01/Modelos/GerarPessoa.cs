1efc9f0b8f85a774368c5d4df31f320f28cbf4ac
César Nagy <103067057+CesarMegas@users.noreply.github.com>
2025-04-02 17:43:43 -0300

    GerarPessoa.cs
    Programa responsável pela criação das Pessoas do Exercício 01.

---
+++ Atividade04/Exercicio01/Modelos/GerarPessoa.cs
@@ -0,0 +1,11 @@
+namespace Atividade04.Modelos
+{
+    internal class GerarPessoa
+    {
+
+        public string? Nome { get; set; }
+        public int Idade { get; set; }
+        public string? Email { get; set; }
+
+    }
+}
