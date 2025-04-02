88a5cd9d06a19152fbb2082026ae19cddfd06196
César Nagy <103067057+CesarMegas@users.noreply.github.com>
2025-04-02 17:42:41 -0300

    Program.cs
    Programa Principal do Exercício 01.

---
+++ Atividade04/Exercicio01/Modelos/Program.cs
@@ -0,0 +1,17 @@
+using Atividade04.Modelos;
+
+GerarPessoa pessoa = new GerarPessoa();
+Console.Write("Digite o Nome da Pessoa: ");
+pessoa.Nome = Console.ReadLine()!;
+Console.Write("Digite a Idade da Pessoa: ");
+pessoa.Idade = int.Parse(Console.ReadLine()!);
+Console.Write("Digite o E-mail da Pessoa: ");
+pessoa.Email = Console.ReadLine()!;
+
+string jsonString = JsonSerializer.Serialize(pessoa);
+
+string fileName = "pessoa.json";
+
+File.WriteAllText(fileName, jsonString);
+
+Console.WriteLine($"Os dados foram salvos em {fileName}");
