ddc74a9c9e095f1820e44c9e2797810beaddd578
César Nagy <103067057+CesarMegas@users.noreply.github.com>
2025-04-02 17:56:31 -0300

    Program.cs
    Programa Principal do Exercício 01.

--- Atividade04/Exercicio01/Program.cs
+++ Atividade04/Exercicio01/Program.cs
@@ -1,3 +1,6 @@
+// Atividade 04 - Criando Arquivos com C#
+// Exercício 01 - Criar um programa que permite ao usuário inserir informações de uma pessoa (nome, idade, e e-mail), serializa essas informações em formato JSON e salva em um arquivo.
+
 using Atividade04.Modelos;
 using System.Text.Json;
 
