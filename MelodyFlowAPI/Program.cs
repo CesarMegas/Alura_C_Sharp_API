f596d6dc42cd3c8fd510a9ac38342bb2a6e52cd4
César Nagy <103067057+CesarMegas@users.noreply.github.com>
2025-04-03 18:06:23 -0300

    Program.cs
    Programa Principal do Projeto.

--- MelodyFlowAPI/Program.cs
+++ MelodyFlowAPI/Program.cs
@@ -10,12 +10,27 @@ using (HttpClient client = new HttpClient())
     {
         string response = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
         var songs = JsonSerializer.Deserialize<List<Song>>(response)!;
-        //songs[1998].DisplaySongDetails();
+        
+        //songs[1].DisplaySongDetails();
         //LinqFilter.FilterEveryMusicalGenre(songs);
         //LinqOrder.DisplayOrderedArtistsList(songs);
         //LinqFilter.FilterArtistsByMusicalGenre(songs, "pop");
         //LinqFilter.FilterSongsOfAnArtist(songs, "Michael Jackson");
-        LinqFilter.FilterSongsByYear(songs, 2000);
+        //LinqFilter.FilterSongsByYear(songs, 2000);
+        LinqFilter.FilterSongsByKey(songs, "C#");
+
+        var cesarsFavoriteSongs = new FavoriteSongs("Cesar");
+        cesarsFavoriteSongs.AddFavoriteSong(songs[1]);
+        cesarsFavoriteSongs.AddFavoriteSong(songs[20]);
+        cesarsFavoriteSongs.AddFavoriteSong(songs[31]);
+        cesarsFavoriteSongs.AddFavoriteSong(songs[35]);
+        cesarsFavoriteSongs.AddFavoriteSong(songs[80]);
+
+        //cesarsFavoriteSongs.DisplayFavoriteSongs();
+
+        //cesarsFavoriteSongs.GenerateJsonFile();
+        //cesarsFavoriteSongs.GenerateTxtFile();
+
     }
     // If it doen't succeed, displays the error
     catch (Exception error) 
